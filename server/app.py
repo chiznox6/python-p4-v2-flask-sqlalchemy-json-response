4da0344eef837b169f8c120c43c11d8a018bd789
James <james.kariuki5@student.moringaschool.com>
2025-09-10 19:02:36 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
+
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
@@ -9,23 +10,56 @@ from models import db, Pet
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False  # Pretty-print JSON
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 
+# Index route returns a welcome message as JSON
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
+    body = {'message': 'Welcome to the pet directory!'}
+    return make_response(body, 200)
 
 
+# Demo route returns a sample pet as JSON
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet_dict = {
+        'id': 1,
+        'name': 'Fido',
+        'species': 'Dog'
+    }
+    return make_response(pet_dict, 200)
+
+
+# Get a pet by its ID
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        body = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species}
+        status = 200
+    else:
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+# Get all pets by species
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = []
+    for pet in Pet.query.filter_by(species=species).all():
+        pets.append({'id': pet.id, 'name': pet.name})
+    
+    body = {'count': len(pets), 'pets': pets}
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
